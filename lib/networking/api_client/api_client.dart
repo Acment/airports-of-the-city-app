ffd914087ea5793b8c261eb6b8ada0c461027a2c
Eduardo Miranda <santose351@gmail.com>
2020-11-25 01:25:16 -0500

    Fetch By filter added

--- lib/networking/api_client/api_client.dart
+++ lib/networking/api_client/api_client.dart
@@ -5,6 +5,7 @@ import 'package:aiports_of_the_city/models/airport_model.dart';
 class GetAirports {
   String _url = 'airports-of-the-world.herokuapp.com';
   int _limit = 10;
+
   // String limit
 
   Future<List<Airport>> fetchAllAirports() async {
@@ -22,6 +23,20 @@ class GetAirports {
 
   Future<List<Airport>> fetchByIATA(String iata) async {
     Uri uri = Uri.https(_url, 'airports/iata/$iata');
+
+    final response = await Dio().getUri(uri);
+
+    final airports = new Airports.fromJsonList(response.data['data']);
+
+    return airports.items;
+  }
+
+  Future<List<Airport>> fetchByFilter(String inputText) async {
+    Uri uri = Uri.https(_url, 'airports',{
+      'name' : inputText,
+      'city' : inputText,
+      'country': inputText,
+    });
 
     final response = await Dio().getUri(uri);
 
