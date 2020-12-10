8648a93bba4d33ebf4d9f239870e285de322d9cf
Eduardo Miranda <santose351@gmail.com>
2020-12-09 21:31:37 -0500

    iata and icao dropdown filter  added

--- lib/views/distance_airports/distance_airports_page.dart
+++ lib/views/distance_airports/distance_airports_page.dart
@@ -5,24 +5,41 @@ import 'package:aiports_of_the_city/models/airport_model.dart';
 import 'package:aiports_of_the_city/models/distance_airports_model.dart';
 import 'package:aiports_of_the_city/views/distance_airports/distance_airports_bloc.dart';
 
-class DistanceAirportsPage extends StatelessWidget {
+class DistanceAirportsPage extends StatefulWidget {
 
+  @override
+  _DistanceAirportsPageState createState() => _DistanceAirportsPageState();
+}
+
+class _DistanceAirportsPageState extends State<DistanceAirportsPage> {
   String origin;
+
+  String codeOrigin;
+
   String destination;
+
+  String codeDestination;
+
+
+  List<String> _codes = ['IATA', 'ICAO'];
 
   @override
   Widget build(BuildContext context) {
   final DistanceAirportsBloc _bloc = BlocProvider.of<DistanceAirportsBloc>(context);
     
     return Scaffold(
+      appBar: AppBar(),
       body: Container(
         margin: EdgeInsets.only(top: 37.0),
         child: Column(
           children: <Widget>[
-            inputsFields(),
+            originRow(),
+            Padding(padding: EdgeInsets.only(top: 15.0)),
+            destinationRow(),
+
             ElevatedButton(
-              onPressed: () => _bloc.add(DistanceSearchEvent(origin: origin, destination: destination)), 
-            child: Text('submmit'),
+              onPressed: () => _bloc.add(DistanceSearchEvent(origin: origin, codeOrigin: codeOrigin, destination: destination, codeDestination: codeDestination)), 
+            child: Text('Search'),
               ),
               Expanded(child: BlocBuilder<DistanceAirportsBloc, DistanceAirportsState>(builder: (context, state){
                 print('state: $state');
@@ -48,43 +65,84 @@ class DistanceAirportsPage extends StatelessWidget {
     );
   }
 
-  Widget inputsFields(){
-    return Column(
-      children: [
-        TextField(
-        autofocus: true,
-        textCapitalization: TextCapitalization.words,
-        decoration: InputDecoration(
-          labelStyle: TextStyle(fontSize: 21.0),
-          labelText: 'Origin',
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-          hintText: 'Write IATA origin code',
-        ),
-        onChanged: (String inputText){
-          if (inputText.length > 0){
-          origin = inputText.trim().toUpperCase();
-          }
-        },
-        ),
-        Padding(padding: EdgeInsets.only(top: 20)),
-        TextField(
-        textCapitalization: TextCapitalization.words,
-        decoration: InputDecoration(
-          labelStyle: TextStyle(fontSize: 21.0),
-          labelText: 'Destination',
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-          hintText: 'Write IATA destination code',
-        ),
-        onChanged: (String inputText){
-          if (inputText.length > 0){
-            destination = inputText.trim().toUpperCase();
-          }
-        },
-        ),
-      ],
-    );
+    List<DropdownMenuItem<String>> getCodeDropdown() {
+    List<DropdownMenuItem<String>> codeList = new List();
 
+    _codes.forEach((code) {
+      codeList.add(DropdownMenuItem(
+        child: Text(code),
+        value: code.toLowerCase(),
+      ));
+    });
+    return codeList;
   }
+
+  Widget originRow(){
+      return Row(children: <Widget>[
+        SizedBox(width: 30.0),
+        DropdownButton(
+            
+            value: codeOrigin==null? codeOrigin:codeOrigin.toLowerCase(),
+            items: getCodeDropdown(),
+            onChanged: (opt) {
+              setState(() {
+                codeOrigin = opt;
+              });
+            },
+          ),
+        Padding(padding: EdgeInsets.only(right: 20.0)),
+          Flexible(
+                      child: TextField(
+            textCapitalization: TextCapitalization.words,
+            decoration: InputDecoration(
+              labelStyle: TextStyle(fontSize: 21.0),
+              labelText: 'Origin',
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              hintText: 'Write origin code',
+            ),
+            onChanged: (String inputText){
+              if (inputText.length > 0){
+                origin = inputText.trim().toUpperCase();
+              }
+            },
+            ),
+          ),
+      ],);
+    }
+
+  Widget destinationRow(){
+      return Row(children: <Widget>[
+        SizedBox(width: 30.0),
+        DropdownButton(
+            
+            value: codeDestination==null? codeDestination:codeDestination.toLowerCase(),
+            items: getCodeDropdown(),
+            onChanged: (opt) {
+              setState(() {
+                codeDestination = opt;
+              });
+            },
+          ),
+        Padding(padding: EdgeInsets.only(right: 20.0)),
+          Flexible(
+                      child: TextField(
+            textCapitalization: TextCapitalization.words,
+            decoration: InputDecoration(
+              labelStyle: TextStyle(fontSize: 21.0),
+              labelText: 'Destination',
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              hintText: 'Write destination code',
+            ),
+            onChanged: (String inputText){
+              if (inputText.length > 0){
+                destination = inputText.trim().toUpperCase();
+              }
+            },
+            ),
+          ),
+      ],);
+    }
+
   Widget displayDistance(Map<String, dynamic> airportsDistance){
     Distance distance = airportsDistance['distance'];
     Airport origin = airportsDistance['origin'];
@@ -116,5 +174,4 @@ class DistanceAirportsPage extends StatelessWidget {
       ],
     );
   }
-  
 }
